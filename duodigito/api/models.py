2fe5524ce1596522c90312caa6c030fe62c074d3
arthurwinck <apwinck@gmail.com>
2021-10-09 18:55:46 -0300

    Fetch do react para a API do Django

--- duodigito/api/models.py
+++ duodigito/api/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-
+#import time
 #Criar uma classe que represente as entradas do usuário no aplicativo, e toda que vez que invocada, calcular
 #o múltiplo duodígito
 
 class EntradaDuodigito(models.Model):
     numero = models.BigIntegerField(default=0)
-    multiplo_duodigito = models.BigIntegerField(default=0)
+    
+    #tempo1 = time.perf_counter()
+    # Cálculo do número duodígito
+    
+
+
+    multiplo_duodigito= models.BigIntegerField(default=0, editable=False)
+    #tempo2 = time.perf_counter()
+    #tempo_final = tempo2 - tempo1
+    # Agora para sabermos o tempo necessário para achar o múltiplo, diminuimos um tempo do outro
+    #tempo_duodigito = models.TimeField(default=0)
     criado_em = models.DateTimeField(auto_now_add=True)
+
--- duodigito/frontend/src/App.js
+++ duodigito/frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import useFetch from './hooks/useFetch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [result, loading] = useFetch('http://127.0.0.1:8000/api');
+
+  if (loading) {
+      return <p>loading...</p>
+  }
+
+  if (!loading && result) {
+
+      return (
+          <div className="App">
+              {result.map(entrada => (
+                <div key={entrada.id}>
+                      <h1>numero={entrada.numero}</h1>
+                      <h1>multiplo={entrada.multiplo_duodigito}</h1>                      
+                </div>
+              ) 
+              )}
+          </div>
+      )
+
+  }
+
+  return <h1>Ocorreu um erro</h1>
 }
 
-export default App;
+export default App
